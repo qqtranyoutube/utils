f72f6317a507bf0ca9a083d337bb53ccc959a233
qqtranyoutube <channel.qqtran@gmail.com>
2025-08-07 10:42:55 -0400

    Update youtube_api.py

--- utils/youtube_api.py
+++ utils/youtube_api.py
@@ -1,1 +1,34 @@
+import requests
+import streamlit as st
+from datetime import datetime, timedelta
+import pandas as pd
 
+def search_meditation_videos_today():
+    api_key = st.secrets["youtube_api_key"]
+    now = datetime.utcnow()
+    published_after = (now - timedelta(days=1)).isoformat("T") + "Z"
+
+    url = "https://www.googleapis.com/youtube/v3/search"
+    params = {
+        "part": "snippet",
+        "q": "meditation",
+        "type": "video",
+        "order": "date",
+        "publishedAfter": published_after,
+        "maxResults": 20,
+        "key": api_key
+    }
+
+    res = requests.get(url, params=params).json()
+    video_data = []
+    for item in res.get("items", []):
+        video = {
+            "videoId": item["id"]["videoId"],
+            "title": item["snippet"]["title"],
+            "channelTitle": item["snippet"]["channelTitle"],
+            "publishedAt": item["snippet"]["publishedAt"],
+            "liveBroadcastContent": item["snippet"]["liveBroadcastContent"],
+            "viewCount": 0  # Placeholder
+        }
+        video_data.append(video)
+    return pd.DataFrame(video_data)
